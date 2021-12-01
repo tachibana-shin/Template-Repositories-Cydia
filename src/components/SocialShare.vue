8dc915a6031256d3976e60af8ed03a882fc61eeb
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-12-01 16:51:37 +0700

    .

--- src/components/SocialShare.vue
+++ src/components/SocialShare.vue
@@ -11,21 +11,22 @@ const items = [
   {
     icon: "/src/assets/icons/github.png",
     name: "Github",
-    to: "#",
+    to: "https://github.com/tachibana-shin",
   },
   {
     icon: "/src/assets/icons/reddit.png",
     name: "Reddit",
-    to: "#",
+    to: "https://reddit.com",
   },
   {
     icon: "/src/assets/icons/twitter.png",
     name: "Follow Tachibana Shin",
-    to: "#",
+    to: "https://twitter.com/tachib_shin",
   },
   {
     icon: "/src/assets/icons/ipa.png",
     name: "Tools Jailbreak",
+    to: "https://tachibana-shin.github.io/jailbreak-tools"
   },
 ];
 </script>
