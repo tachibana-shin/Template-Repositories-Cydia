0dad3b2f51686ba78d2aad5db9d30897801a75aa
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-12-01 16:06:25 +0700

    .

--- ftparchive-build.sh
+++ ftparchive-build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if ! [ -d ./public ]; then
+  mkdir ./public
+fi
+
 apt-ftparchive packages "./debian" > public/Packages
-bzip2 -c9k Packages > public/Packages.bz2
-gzip -c Packages > public/Packages.gz
+bzip2 -c9k public/Packages > public/Packages.bz2
+gzip -c public/Packages > public/Packages.gz
+
+exit 0
