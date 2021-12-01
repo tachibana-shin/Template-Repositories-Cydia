7cfa13f11f14fc9ebae6ae93230a30e6dc7388f2
tachibana-shin <thanhnguyennguyen1995@gmail.com>
2021-12-01 14:34:21 +0700

    add sections

--- src/components/Depiction.vue
+++ src/components/Depiction.vue
@@ -119,7 +119,7 @@
   </div>
 
   <PackageUpdates />
-  <SectionList />
+  <Sections />
   <SocialShare />
   <ThemeChange />
   <Copyright />
---
+++ src/components/Sections.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListItemGroup name="Sections" :items="items" />
+</template>
+
+<script lang="ts" setup>
+  import list from "pages/section/list.json"
+  import { usePackageIcon } from "../uses/packageIcon"
+
+  const items = list.map(item => {
+    return {
+      name: item.name,
+      icon: usePackageIcon(null, item.name),
+      to: item.to
+    }
+  })
+</script>
