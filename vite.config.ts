d884f00aae4ca3c4e2c87c9eefa3c9cde7284105
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-12-04 11:38:42 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import { slugify } from "./scripts/slugify";
 import yaml from "yamljs";
 
 const config: UserConfig = {
-  base: "/web-repo",
+  base: "/web-repo/",
   resolve: {
     alias: {
       assets: join(__dirname, "src/assets"),
